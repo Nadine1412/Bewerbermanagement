ae71faf9284bfcae3134f198642aa5c7fedc688c
Nadine1412 <nadine.jakob@hs-furtwangen.de>
2019-06-07 15:28:43 +0200

    Login statement angepasst. Nachname wird selektiert

--- src/de/hfu/bewerbermanagement/dao/UserDao.java
+++ src/de/hfu/bewerbermanagement/dao/UserDao.java
@@ -9,7 +9,7 @@ public interface UserDao {
 	
 	public int registerBewerber(Bewerber bewerber);
 
-	public int loginUser(User user);
+	public String loginUser(User user);
 	
 	// Profil anzeigen
 	public Bewerber showProfil(HttpSession session);
